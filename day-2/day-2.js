95687ec0a8f9d2735d20ecd3410f9394830a8dc5
drdytftfsdtf <157824717+drdytftfsdtf@users.noreply.github.com>
2024-02-02 00:24:19 +0530

    UPDATE

---
+++ day-2/day-2.js
@@ -0,0 +1,26 @@
+{
+    let myname  = "sajid";
+    console.log(myname)
+
+}
+{
+    const mysurname = "ansari";
+    console.log(mysurname)
+}
+// console.log(myname)
+//console.log(mysurname)
+
+// datatype
+// varibles
+// operator
+
+// comparision operator
+{/* < > < = >= == ===*/}
+console.log(3 < 1)
+console.log(5 > 4)
+console.log(3 <= 3)
+console.log(4 >= 4)
+console.log(5 == 5)
+console.log(6 == "6")
+console.log(9 === 9)
+console.log(8 === "8")
