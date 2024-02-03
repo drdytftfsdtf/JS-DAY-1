3a9ff659f2814ecb9d1a00d13b35a9c2d5e3505b
drdytftfsdtf <157824717+drdytftfsdtf@users.noreply.github.com>
2024-02-03 11:55:44 +0530

    UPDATE

---
+++ day-3/day-3.js
@@ -0,0 +1,28 @@
+
+// value1=30;
+// value2=16:
+// value3=25;
+
+// if (value1>value2){
+//    console.log(
+//   "val1 is bigger then val 2"
+//    )
+// }  else if (value1>value3){
+//    console.log("val 1 is bigger then val 2 and 3")    
+// }else (
+//  console.log("value3 is bigger")
+// )
+
+
+var age =79;
+var min =18;
+var max =80;
+
+if (age>min && age<max)
+{
+    console.log("you are eligible for liscence")
+}else if (age==18) {
+    console.log("you are eligible for learning liscence")
+}else {
+    console.log("you are  not eligible for liscence")
+}
