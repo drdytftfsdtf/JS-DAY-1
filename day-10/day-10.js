d39f3f603c067f6b1a2b664626ba34b24f532246
drdytftfsdtf <157824717+drdytftfsdtf@users.noreply.github.com>
2024-02-18 01:21:56 +0530

    update

---
+++ day-10/day-10.js
@@ -0,0 +1,41 @@
+//  array map function
+
+ var array= [1,8,2,3,4,5,6]
+ const result= array.map((num)=> num> 5)
+ console.log(result)
+
+
+
+
+// var array= [1,8,2,3,4,5,6]
+// const res = array.forEach((Number) => (number > 5))
+// console.log(res,"res")
+
+
+// push array
+
+// var array =[1,2,3,4,5,6,8,]
+// const newindec = array. Push(6)
+// console.log(newindec,"newindec")
+// console.log(array)
+
+
+
+
+var array = [1,2,3]
+array .pop() 
+console.log(array.pop())
+console.log(array)
+
+
+
+
+
+var array =['a','b','c','d','e' ]
+
+array.splice(1,0,'z')
+console.log(array)
+
+
+array.splice(4,1,'x')
+console.log(array)
---
+++ day-11/day-11.js
@@ -0,0 +1,35 @@
+var name ="sajid khan"
+var style = "fast bowler"
+console.log("virat koli - ",name)
+console.log(`${name}is a ${style}`)
+
+
+var array1=[1,2,3,4]
+var array2=[5,6,7,8]
+const result =array1.concat(array2)
+console.log(result)
+
+
+
+ var source= ["car","bike","scooty"]
+const lexists = source.includes("cycle")
+console.log (lexists);
+
+
+
+var array =[1,2,3,4]
+const lexist = array.includes(2)
+console.log(lexist);
+
+
+
+var number =[1,2,3,300,5,6]
+console.log(number.sort())
+
+
+var number =[1,5,200,3,2,4]
+console.log(number.sort((a , b) => a-b))
+console.log(number.sort((a , b) => a-b))
+
+
+console.log 
